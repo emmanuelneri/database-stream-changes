73e84240f528b1dfb429cf1c98093a0a21ac93f3
emmanuelneri <emmanuelnerisouza@gmail.com>
2019-09-28 18:19:28 -0300

    adjusting docker command

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 kafkaURL=kafka:29092
 kafkaConnectorURL=http://localhost:8083/connectors
 
-docker exec -it kafka kafka-topics --create --bootstrap-server ${kafkaURL} --replication-factor 1 --partitions 1 --topic db.public.customer
+docker exec -d kafka kafka-topics --create --bootstrap-server ${kafkaURL} --replication-factor 1 --partitions 1 --topic db.public.customer
 docker exec -d kafka kafka-topics --create --bootstrap-server ${kafkaURL} --replication-factor 1 --partitions 1 --topic db.public.sale
 
 curl -v POST -H "Content-Type: application/json" -d @setup/connector/debezium-kafka-connector.json ${kafkaConnectorURL}
